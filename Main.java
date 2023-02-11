d25974306621696010990f5a88fb21db436ff5f5
hype1356 <swayamkan@gmail.com>
2023-02-11 10:35:07 +0000

    get rid of test function

--- Main.java
+++ Main.java
@@ -2,8 +2,4 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("hello world!");
   }
-
-  public static void hello(){
-    System.out.println("Hello, World!");
-  }
 }
