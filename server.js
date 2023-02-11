4db18189db4cde8f7c9f4a46d561b96c1f208623
FrostyMan12345 <roberto.mari04@outlook.com>
2023-02-11 19:09:08 +0000

    dictionary done

--- server.js
+++ server.js
@@ -9,19 +9,14 @@ function readCSVFile() {
     var lines = input.toString().split('\n');
     var dictionary = new Object();
     for (var count = 0; count<lines.length - 1; count++) {
-      console.log(lines[count] + " " + count );
       var buildingNumber = lines[count].split(',')[0];
-      console.log(lines[count].split(',', 1)[1]);
-      var line = [];
-      for (var count2 = 0; count2<lines[count].split(',').length; count2++) {
-        if (count2 != 0) {
-          line.push(lines[count].split(',')[count2]);
-        }
-      }
+      var line = lines[count].split(',');
+      line[line.length-1] = line[line.length-1].split('\r')[0];
+      line.shift(); 
       for (let number of line) {
         console.log(number);
       }
-      
+      dictionary[buildingNumber] = line;
     }
   })
 }
